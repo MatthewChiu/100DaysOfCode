4681cc83cd0dd54310ac8a03e4991dd06ad87058
Matt Chiu <55036083+MatthewChiu@users.noreply.github.com>
2021-03-09 11:55:32 -0500

    Create Day4.5.CPCCStart.py

---
+++ Day4.5.CPCCStart.py
@@ -0,0 +1,5 @@
+# Presented at the Future Directions of Music Cognition with Noah (Kahrs): Chord Spacing and Quality. 
+# We didn't receive many questions, but the slack channel was popping and there were a bunch of awesome talks (open access!).
+
+# In addition to a bit more solo coding exercises, I edited a bit of my website and started construction on the website for the CPCC (see its github page).
+# So today was mostly HTML (and a little bit of CSS), but no JS.
