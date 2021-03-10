4d2d39ecda2ea5887339cf0b5a2a53e510ae30cd
Matt Chiu <55036083+MatthewChiu@users.noreply.github.com>
2021-03-09 20:34:39 -0500

    Create Day6.WineBoxy.py

---
+++ Day6.WineBoxy.py
@@ -0,0 +1,4 @@
+# In terms of real work: I tested various weightings for the NN of metric types (looks like it averages between 70–82% accurac).
+# In terms of real fun: I created an app for evaluating how "wine boxy" we are (per Lukas Perry) using the fourier transform.
+
+# (For source code, see google colab)
