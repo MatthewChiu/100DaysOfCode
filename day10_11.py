fa8e5583e53d4ae17a0d302105b0fd5415e7c94f
Matt Chiu <55036083+MatthewChiu@users.noreply.github.com>
2021-03-22 14:26:32 -0400

    Create day10_11.py

---
+++ day10_11.py
@@ -0,0 +1,5 @@
+# Sooooo technically there were a few days that went by where I was coding/learning through codeacademy, but not doing much other than that. 
+# We visited Eron's family in GA, so I didn't have that much time... but we had great family time!
+
+# Over the past days, I worked more on react and did a bunch of things for the catFoodCounter:
+# Counter active; at the end of a day, subtract 4 cans; add 24,32,40 cans or 1 up/down at a time.
